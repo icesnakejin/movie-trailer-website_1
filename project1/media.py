03e5d6411c70d73985f00cc078d12a5d1c01220d
icesnakejin <icesnakejin@gmail.com>
2015-04-22 23:02:57 -0700

    Committed by Udacity

---
+++ project1/media.py
@@ -0,0 +1,9 @@
+# the class for movie, instance variables: title, story line ,image and trailer url.
+
+class Movie():
+    def __init__(self,movie_title, movie_storyline, poster_image, trailer_youtube):
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
+
